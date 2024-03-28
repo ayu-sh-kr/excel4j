0a1284afa43db51b43f02bda7261a9b6bde349bb
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-28 22:10:35 +0530

    Excel4j initial commit
    Excel4j is java library to read and write excel based worksheet

---
+++ src/main/java/dev/archimeded/excel4j/Main.java
@@ -0,0 +1,7 @@
+package dev.archimeded.excel4j;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/dev/archimeded/excel4j/annotations/ExcelSheet.java
@@ -0,0 +1,15 @@
+package dev.archimeded.excel4j.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface ExcelSheet {
+
+    String name() default "";
+    int sheetNumber() default 0;
+
+}
