8db7ba2ab741acda5df6f2c515c16450ba807ad0
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-30 00:56:02 +0530

    Base method will use InputStream to read from excel

--- src/main/java/dev/archimeded/excel4j/processor/contracts/ExcelReader.java
+++ src/main/java/dev/archimeded/excel4j/processor/contracts/ExcelReader.java
@@ -2,13 +2,13 @@ package dev.archimeded.excel4j.processor.contracts;
 
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 public interface ExcelReader<T> {
 
     List<T> read(File file, Class<T> clazz);
 
-    List<T> read(FileInputStream fileInputStream, Class<T> clazz);
+    List<T> read(InputStream inputStream, Class<T> clazz);
 
 }
