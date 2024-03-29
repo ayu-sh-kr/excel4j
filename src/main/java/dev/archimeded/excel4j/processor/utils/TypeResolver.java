ff2b329fefdd5c526d0e88d86d4ac116496ec4c6
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-03-30 00:55:03 +0530

    Created type resolver to resolve the numeric and boolean cell type to respective field type

---
+++ src/main/java/dev/archimeded/excel4j/processor/utils/TypeResolver.java
@@ -0,0 +1,29 @@
+package dev.archimeded.excel4j.processor.utils;
+
+import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.lang.reflect.Field;
+
+public class TypeResolver {
+
+    public static void resolveString(T instance, Field field, Cell cell) throws IllegalAccessException {
+        field.set(instance, cell.getStringCellValue());
+    }
+
+    public static void resolveNumber(T instance, Field field, Cell cell) throws IllegalAccessException {
+        switch (field.getType().getSimpleName()){
+            case "Long" -> field.setLong(instance, (long) cell.getNumericCellValue());
+            case "Integer" -> field.setInt(instance, (int) cell.getNumericCellValue());
+            case "Double" -> field.setDouble(instance,  cell.getNumericCellValue());
+            case "Float" -> field.setFloat(instance, (float) cell.getNumericCellValue());
+            case "Date" -> field.set(instance, cell.getDateCellValue());
+            case "LocalDateTime" -> field.set(instance, cell.getLocalDateTimeCellValue());
+            default -> throw new IllegalStateException("Unexpected value: " + field.getType());
+        }
+    }
+
+    public static void resolveBoolean(T instance, Field field, Cell cell) throws IllegalAccessException {
+        field.setBoolean(instance, cell.getBooleanCellValue());
+    }
+}
