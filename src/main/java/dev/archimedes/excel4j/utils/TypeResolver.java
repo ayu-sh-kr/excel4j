d5cd539b4f8fdb4ed5f60334b2aac7417f88a8e3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-04-12 23:50:13 +0530

    Update TypeResolver.java: Updated resolveString() method to resolve the all cell type to String, update resolveDate methods to resolve date correctly and skip null values

--- src/main/java/dev/archimedes/excel4j/utils/TypeResolver.java
+++ src/main/java/dev/archimedes/excel4j/utils/TypeResolver.java
@@ -2,6 +2,7 @@ package dev.archimedes.excel4j.utils;
 
 import dev.archimedes.excel4j.options.ExcelOption;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.util.StringUtil;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
@@ -10,14 +11,19 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Date;
 
 public class TypeResolver {
 
-    public static <T> void resolveString(T instance, Field field, Cell cell) throws IllegalAccessException {
-        field.set(instance, cell.getStringCellValue());
+    public static <T> void resolveString(T instance, Field field, Cell cell) {
+        try {
+            field.set(instance, cell.toString());
+        }catch (IllegalAccessException | IllegalStateException e){
+            throw new RuntimeException(ResolveExceptionLog.prepare(field, cell, e.getLocalizedMessage()), e.getCause());
+        }
     }
 
     public static Number resolveNumber(Cell cell) throws ParseException {
@@ -107,26 +113,53 @@ public class TypeResolver {
         }
     }
 
-    public static <T> void resolveDate(T instance, Field field, Cell cell, ExcelOption<T> option) throws ParseException, IllegalAccessException {
-        switch (cell.getCellType()) {
-            case NUMERIC -> field.set(instance, cell.getDateCellValue());
-            case STRING -> {
-                SimpleDateFormat format = new SimpleDateFormat(option.getDateTimeRegex());
-                Date date = format.parse(cell.getStringCellValue());
-                field.set(instance, date);
+    public static <T> void resolveDate(T instance, Field field, Cell cell, int rowIdx, ExcelOption<T> option) throws ParseException, IllegalAccessException {
+        SimpleDateFormat format = new SimpleDateFormat(option.getDateTimeRegex());
+        try {
+            switch (cell.getCellType()) {
+                case NUMERIC -> {
+                    if(StringUtil.isNotBlank(String.valueOf(cell.getNumericCellValue()))){
+                        field.set(instance, cell.getDateCellValue());
+                    }
+                }
+                case STRING -> {
+                    if(StringUtil.isNotBlank(cell.getStringCellValue())){
+                        Date date = format.parse(cell.getStringCellValue());
+                        field.set(instance, date);
+                    }
+                }
             }
+        }catch (ParseException | IllegalAccessException | IllegalStateException e){
+            throw new RuntimeException(ResolveExceptionLog.prepare(field, cell, e.getLocalizedMessage()) + " row: " + rowIdx, e.getCause());
         }
     }
 
 
     public static <T> void resolveLocalDate(T instance, Field field, Cell cell, ExcelOption<T> option) throws IllegalAccessException {
-        switch (cell.getCellType()) {
-            case NUMERIC -> field.set(instance, cell.getDateCellValue());
-            case STRING -> {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(option.getDateRegex());
-                LocalDate date = LocalDate.parse(cell.getStringCellValue(), formatter);
-                field.set(instance, date);
+        try {
+            switch (cell.getCellType()) {
+                case NUMERIC -> {
+                    if (StringUtil.isNotBlank(String.valueOf(cell.getNumericCellValue()))){
+                        field.set(
+                                instance, cell.getDateCellValue()
+                                        .toInstant()
+                                        .atZone(ZoneId.systemDefault())
+                                        .toLocalDate()
+                        );
+                    }
+                }
+                case STRING -> {
+                    if(StringUtil.isNotBlank(cell.getStringCellValue())){
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(option.getDateRegex());
+                        LocalDate date = LocalDate.parse(cell.getStringCellValue(), formatter);
+                        field.set(instance, date);
+                    }
+                }
             }
+        }catch (Exception e){
+            throw new RuntimeException(ResolveExceptionLog.prepare(field, cell, e.getLocalizedMessage()));
         }
     }
 }
+
+// "", Numeric, String, Boolean 10-12-23 8463434.343 epoch, days
